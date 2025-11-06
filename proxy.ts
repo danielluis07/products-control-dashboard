3679f2369aff611aec67cda9a3b3103b383c0717
danielluis07 <111083286+danielluis07@users.noreply.github.com>
2025-11-06 07:15:48 -0300

    refactor: streamline session handling and route redirection logic

--- proxy.ts
+++ proxy.ts
@@ -2,27 +2,42 @@ import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
 const LOGIN_ROUTE = "/login";
+const ROOT_ROUTE = "/";
+const REGISTER_ROUTE = "/register";
+const DASHBOARD_ROUTE = "/dashboard/admin";
 
 export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   const sessionCookie = getSessionCookie(request);
 
-  if (pathname.startsWith(LOGIN_ROUTE) && sessionCookie) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
+  if (sessionCookie) {
+    if (
+      pathname === ROOT_ROUTE ||
+      pathname === LOGIN_ROUTE ||
+      pathname === REGISTER_ROUTE
+    ) {
+      return NextResponse.redirect(new URL(DASHBOARD_ROUTE, request.url));
+    }
   }
 
-  if (pathname.startsWith("/dashboard") && !sessionCookie) {
-    const searchParams = new URLSearchParams();
-    searchParams.set("callbackUrl", pathname);
-    return NextResponse.redirect(
-      new URL(`${LOGIN_ROUTE}?${searchParams.toString()}`, request.url)
-    );
+  if (!sessionCookie) {
+    if (pathname === ROOT_ROUTE || pathname === REGISTER_ROUTE) {
+      return NextResponse.redirect(new URL(LOGIN_ROUTE, request.url));
+    }
+
+    if (pathname.startsWith("/dashboard")) {
+      const searchParams = new URLSearchParams();
+      searchParams.set("callbackUrl", pathname);
+      return NextResponse.redirect(
+        new URL(`${LOGIN_ROUTE}?${searchParams.toString()}`, request.url)
+      );
+    }
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/login"],
+  matcher: ["/", "/dashboard/:path*", "/login", "/register"],
 };
